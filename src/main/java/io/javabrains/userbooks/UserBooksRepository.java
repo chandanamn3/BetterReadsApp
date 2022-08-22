a35e55687561083397b4a6829e4bbff9abe62339
chandana <chandanamn07@gmail.com>
2022-08-23 00:39:42 +0530

    Add Book by User and Dashboard functionality

---
+++ src/main/java/io/javabrains/userbooks/UserBooksRepository.java
@@ -0,0 +1,12 @@
+package io.javabrains.userbooks;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBooksPrimaryKey> {
+
+    
+}
+    
+
